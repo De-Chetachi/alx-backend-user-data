53c97e1cf2aa98a7c0b3d4df7214cf65b96e5db1
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-07-15 22:17:58 +0100

    ak

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,11 @@
 '''filter data to conceal personnal data'''
 from typing import List
 import re
+import logging
 
 
 def filter_datum(
-        fields: List[str], redaction: str, message: str, seperator: str
+        fields: List[str], redaction: str, message: str, separator: str
         ) -> str:
     '''obfuscates(make obscure unintelligible) data
     params:
@@ -16,6 +17,23 @@ def filter_datum(
         all fields in the log line (message)
     '''
     for field in fields:
-        pattern = field + f"=.*?{seperator}"
-        message = re.sub(pattern, field + "=" + redaction + seperator, message)
+        pattern = field + f"=.*?{separator}"
+        message = re.sub(pattern, field + "=" + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields;
+
+    def format(self, record: logging.LogRecord) -> str:
+        log = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
