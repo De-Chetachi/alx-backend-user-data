b871736c58441b4a80e6b80fa23d16742af4ee5a
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-07-20 18:42:42 +0100

    ok

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -32,7 +32,7 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields;
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         '''format documentation'''
