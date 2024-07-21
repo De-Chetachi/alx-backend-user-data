4d54a4a9728716ef14f6f865a218464262307c64
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-07-21 22:27:20 +0100

    ok

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''authentication module'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''my auth class'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''require authentication'''
+        if not path or not excluded_paths or not len(excluded_paths):
+            return True
+
+        if not path.endswith('/'):
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+
+    def authorization_header(self, request=None) -> str:
+        '''require authentication'''
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''retrive user making a request'''
+        return None
