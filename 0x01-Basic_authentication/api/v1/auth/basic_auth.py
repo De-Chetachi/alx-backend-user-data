2d2cd78aab739cd76a4e3d8d459900740f93337c
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-08-05 20:43:08 +0100

    ok

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 '''basic uth module'''
 from api.v1.auth.auth import Auth
-
+from base64 import b64decode
 
 class BasicAuth(Auth):
     '''a basic auth class inherits fron auth above'''
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        ''' returns the Base64 part of the Authorization header'''
+        if not authorization_header:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.replace('Basic ', '')
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        '''eturns the decoded value of a Base64 string
+        base64_authorization_header'''
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            return b64decode(base64_authorization_header)
+        except:
+            return None
+
