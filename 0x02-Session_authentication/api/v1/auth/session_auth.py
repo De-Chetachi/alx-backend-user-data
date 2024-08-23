6173d9904c4986039f1e70f211549931c6ed94ff
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-08-23 22:01:49 +0100

    ok

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
     
     def create_session(self, user_id: str = None) -> str:
+        '''creates a session id and stores it'''
         if not user_id:
             return None
         if type(user_id) != str:
