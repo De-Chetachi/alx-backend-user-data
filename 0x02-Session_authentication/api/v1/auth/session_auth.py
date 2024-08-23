74a03e2673788091e5442b584bb19a1bd7a706d0
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-08-23 22:22:23 +0100

    ok

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,6 +16,16 @@ class SessionAuth(Auth):
             return None
         if type(user_id) != str:
             return None
-        session_id = uuid4()
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''returns a user id based on the session'''
+        print(type(session_id))
+        if not session_id:
+            return None
+        if type(session_id) != str:
+            return None
+        res = self.user_id_by_session_id.get(session_id)
+        return res
