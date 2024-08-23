44dfb2b581c4926792846e2ace9b1e8792c1d67b
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-08-23 21:57:09 +0100

    ok

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''docu ment
+session auth module
+this module contains a SessionAuth class'''
+from api.v1.auth.auth import Auth
+from uuid import uuid4
+
+class SessionAuth(Auth):
+    '''inherits from auth'''
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        if not user_id:
+            return None
+        if type(user_id) != str:
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
